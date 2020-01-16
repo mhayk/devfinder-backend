96a48e8a8f85bb18914b2916e81799688bcbe706
Mhayk Whandson <hi@mhayk.com>
2020-01-16 10:21:14 +0000

    The cors package has been installed and configurated.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const routes = require('./routes')
 const app = express()
 
@@ -8,6 +9,7 @@ mongoose.connect('mongodb+srv://devradar:devradarpassword@cluster0-anhhw.mongodb
   useUnifiedTopology: true
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
